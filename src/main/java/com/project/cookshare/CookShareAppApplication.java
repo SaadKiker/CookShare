abfe5c1efa2a281b3291a5508fff7ddc8aff996b
Saad <saadkiker.k@gmail.com>
2024-01-17 14:04:01 -0500

    First commit

---
+++ src/main/java/com/project/cookshare/CookShareAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.cookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookShareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookShareAppApplication.class, args);
+	}
+
+}
